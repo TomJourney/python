9027b4670248d8838fef3b11ea9e18db3d01b4eb
TomJourney <pacosongreen@gmail.com>
2026-03-15 21:20:17 +0800

    invoke and format method.

--- third_poetry_demo/src/poetry_demo/heima_rag/code/0312_langchain_fewshot_prompt_template.py
+++ third_poetry_demo/src/poetry_demo/heima_rag/code/0312_langchain_fewshot_prompt_template.py
@@ -1,31 +1,21 @@
-# langchain少样本提示词模版
-from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
-from langchain_community.llms.tongyi import Tongyi
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import FewShotPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
-example_prompt_template = PromptTemplate.from_template("单词：{word}, 反义词：{antonym}")
+"""
+PromptTemplate ->(extends) StringPromptTemplate -> BasePromptTemplate -> RunnableSerializable -> Runnable
+FewShotPromptTemplate -> StringPromptTemplate -> BasePromptTemplate -> RunnableSerializable -> Runnable
+ChatPromptTemplate -> BaseChatPromptTemplate -> BasePromptTemplate -> RunnableSerializable -> Runnable
+"""
 
-example_data = [
-    {"word":"大", "antonym":"小"},
-    {"word":"上", "antonym":"下"}
-]
+# 测试format
+template = PromptTemplate.from_template("我的邻居是 {lastname}，最喜欢{hobby}")
+result = template.format(lastname="张三", hobby="钓鱼")
+print(result) # 我的邻居是 张三，最喜欢钓鱼
+print(type(result)) # <class 'str'>
 
-# 少样本提示词模板FewShotPromptTemplate， 封装了通用模板对象PromptTemplate
-few_shot_prompt = FewShotPromptTemplate(
-    example_prompt=example_prompt_template, # 提示词模板
-    examples=example_data,  # 示例数据 ，用于注入动态数据 ， list内套字典
-    prefix="返回给定词的反义词，有如下示例：", # 示例数据之前的提示词
-    suffix="基于示例告诉我，{input_word}的反义词是什么",  # 示例数据之后的提示词
-    input_variables=['input_word']
-)
+# 测试invoke
+result2 = template.invoke({"lastname":"李四", "hobby":"唱歌"})
+print(result2) # text='我的邻居是 李四，最喜欢唱歌'
+print(type(result2)) # <class 'langchain_core.prompt_values.StringPromptValue'>
 
-# 获得最终提示词
-prompt_text = few_shot_prompt.invoke(input={"input_word":"左"})
-print(prompt_text.to_string())
-# 单词：大, 反义词：小
-# 单词：上, 反义词：下
-# 基于示例告诉我，左的反义词是什么
-
-# 调用大模型
-model = Tongyi(model="qwen-max")
-result = model.invoke(input=prompt_text)
-print(result)  # 基于您给出的示例，"左" 的反义词是 "右"。
--- third_poetry_demo/src/poetry_demo/heima_rag/heima_rag_studynote.md
+++ third_poetry_demo/src/poetry_demo/heima_rag/heima_rag_studynote.md
@@ -1663,7 +1663,39 @@ print(prompt_text.to_string())
 
 ### 【3.13.1】format与invoke方法代码实现
 
+【0313_langchain_prompt_template_format_invoke.py】
 
+```python
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import FewShotPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+
+"""
+PromptTemplate ->(extends) StringPromptTemplate -> BasePromptTemplate -> RunnableSerializable -> Runnable
+FewShotPromptTemplate -> StringPromptTemplate -> BasePromptTemplate -> RunnableSerializable -> Runnable
+ChatPromptTemplate -> BaseChatPromptTemplate -> BasePromptTemplate -> RunnableSerializable -> Runnable
+"""
+
+# 测试format
+template = PromptTemplate.from_template("我的邻居是 {lastname}，最喜欢{hobby}")
+result = template.format(lastname="张三", hobby="钓鱼")
+print(result) # 我的邻居是 张三，最喜欢钓鱼
+print(type(result)) # <class 'str'>
+
+# 测试invoke
+result2 = template.invoke({"lastname":"李四", "hobby":"唱歌"})
+print(result2) # text='我的邻居是 李四，最喜欢唱歌'
+print(type(result2)) # <class 'langchain_core.prompt_values.StringPromptValue'>
+```
+
+【<font color=red>format与invoke的应用场景</font>】
+
+- 若需要使用langchain的链，则使用invoke返回的提示词是一个对象StringPromptValue；
+- 若需要使用字符串类型的提示词，则使用format方法返回提示词； 
+
+<br>
+
+---
 
 
 
