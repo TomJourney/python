45be0c588fb1627e9f2b58bd201950b60a50beaf
TomJourney <pacosongreen@gmail.com>
2026-03-25 22:14:17 +0800

    update knowledge base.

--- third_poetry_demo/src/poetry_demo/heima_rag/ragproj/config_data.py
+++ third_poetry_demo/src/poetry_demo/heima_rag/ragproj/config_data.py
@@ -1,1 +1,11 @@
 md5_path = "./md5.text"
+
+# chroma
+collection_name="rag"
+persist_directory="./chroma_db"
+
+# 文本分割器-splitter
+chunk_size=1000
+chunk_overlap=100
+separators=["\n\n", "\n", ".", "!", "?", "。", "？", "[", "]", "", " "]
+max_split_char_number = 1000 # 文本分割的阈值
