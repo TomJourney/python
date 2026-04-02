25cf89e04ea820ce50adb8739930f0a63a02cb36
TomJourney <pacosongreen@gmail.com>
2026-04-03 07:01:13 +0800

    langchain agent react framework.

--- third_poetry_demo/src/poetry_demo/heima_rag/agent/test0504_agent_react.py
+++ third_poetry_demo/src/poetry_demo/heima_rag/agent/test0504_agent_react.py
@@ -3,23 +3,26 @@ from langchain_community.chat_models.tongyi import ChatTongyi
 from langchain_core.tools import tool
 
 # 获取股票价格工具或函数
-@tool(description="获取股票价格工具，传入股票名称，返回字符串信息")
-def get_price(name: str) -> str:
-    return f"股票{name}的价格是20元"
+@tool(description="获取体重，返回值是整数，单位千克")
+def get_weight(name: str) -> str:
+    return 90
 
 # 获取股票信息
-@tool(description="获取股票信息，传入股票名称，返回字符串信息")
-def get_info(name: str) -> str:
-    return f"股票{name}是一家A股上市公司，专注于IT职业教育"
+@tool(description="获取身高，返回值是整数，单位厘米")
+def get_height(name: str) -> str:
+    return 172
 
 agent = create_agent(
     model=ChatTongyi(model="qwen3-max"),
-    tools=[get_price, get_info],  # 向智能体提供工具列表
-    system_prompt="你是一个智能助手，可以回答股票相关问题，记住请告知我思考过程，让我知道你为什么调用某个工具"
+    tools=[get_weight, get_height],  # 向智能体提供工具列表
+    system_prompt="""你是严格遵循ReAct框架的智能体，必须按照[思考->行动->观察->再思考]的流程解决问题。
+    且**每轮仅能思考并调用1个工具**，禁止单次调用多个工具。
+    并告诉我你的思考过程，工具的调用原因。按思考、行动、观察三个结构告知我
+    """
 )
 
 for chunk in agent.stream(
-    {"messages":[{"role":"user", "content":"传智教育股价多少，并介绍下"}]},
+    {"messages":[{"role":"user", "content":"计算我的BMI"}]},
     stream_mode="values"
 ):
     latest_msg = chunk['messages'][-1]
@@ -32,3 +35,32 @@ for chunk in agent.stream(
             print(f"工具调用：{ [tc['name'] for tc in latest_msg.tool_calls] }")
     except AttributeError as e:
         pass
+
+# HumanMessage 计算我的BMI
+# AIMessage [思考]
+# 要计算BMI（身体质量指数），需要知道用户的体重（千克）和身高（厘米）。公式为：
+# $$ \text{BMI} = \frac{\text{体重（kg）}}{(\text{身高（m）})^2} $$
+# 因此，首先需要获取用户的体重或身高。由于两个数据都未知，我先选择获取体重。
+#
+# [行动]
+#
+# 工具调用：['get_weight']
+# ToolMessage 90
+# AIMessage [观察]
+# 获取到用户的体重为90千克。
+#
+# [思考]
+# 现在已知体重为90千克，接下来需要获取用户的身高（厘米），以便计算BMI。因此，下一步调用获取身高的工具。
+#
+# [行动]
+#
+#
+# 工具调用：['get_height']
+# ToolMessage 172
+# AIMessage [观察]
+# 获取到用户的身高为172厘米。
+#
+# [思考]
+# 现在已知体重为90千克，身高为172厘米。根据BMI公式：
+# $$ \text{BMI} = \frac{90}{(1.72)^2} \approx 30.3 $$
+# 计算得出用户的BMI约为30.3，属于肥胖范围（BMI ≥ 30）。
--- third_poetry_demo/src/poetry_demo/heima_rag/heima_rag_studynote.md
+++ third_poetry_demo/src/poetry_demo/heima_rag/heima_rag_studynote.md
@@ -4371,7 +4371,80 @@ AIMessage 传智教育的当前股价是 **20元**。
 
 1. langchain的Agent对象遵循ReAct框架要求，在执行过程中会持续自我思考，自我行动，自我观察；
 
+【代码实现】test0504_agent_react.py
 
+```python
+from langchain.agents import create_agent
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.tools import tool
+
+# 获取股票价格工具或函数
+@tool(description="获取体重，返回值是整数，单位千克")
+def get_weight(name: str) -> str:
+    return 90
+
+# 获取股票信息
+@tool(description="获取身高，返回值是整数，单位厘米")
+def get_height(name: str) -> str:
+    return 172
+
+agent = create_agent(
+    model=ChatTongyi(model="qwen3-max"),
+    tools=[get_weight, get_height],  # 向智能体提供工具列表
+    system_prompt="""你是严格遵循ReAct框架的智能体，必须按照[思考->行动->观察->再思考]的流程解决问题。
+    且**每轮仅能思考并调用1个工具**，禁止单次调用多个工具。
+    并告诉我你的思考过程，工具的调用原因。按思考、行动、观察三个结构告知我
+    """
+)
+
+for chunk in agent.stream(
+    {"messages":[{"role":"user", "content":"计算我的BMI"}]},
+    stream_mode="values"
+):
+    latest_msg = chunk['messages'][-1]
+
+    if latest_msg.content:
+        print(type(latest_msg).__name__, latest_msg.content)
+
+    try:
+        if latest_msg.tool_calls:
+            print(f"工具调用：{ [tc['name'] for tc in latest_msg.tool_calls] }")
+    except AttributeError as e:
+        pass
+
+# HumanMessage 计算我的BMI
+# AIMessage [思考]
+# 要计算BMI（身体质量指数），需要知道用户的体重（千克）和身高（厘米）。公式为：
+# $$ \text{BMI} = \frac{\text{体重（kg）}}{(\text{身高（m）})^2} $$
+# 因此，首先需要获取用户的体重或身高。由于两个数据都未知，我先选择获取体重。
+#
+# [行动]
+#
+# 工具调用：['get_weight']
+# ToolMessage 90
+# AIMessage [观察]
+# 获取到用户的体重为90千克。
+#
+# [思考]
+# 现在已知体重为90千克，接下来需要获取用户的身高（厘米），以便计算BMI。因此，下一步调用获取身高的工具。
+#
+# [行动]
+#
+#
+# 工具调用：['get_height']
+# ToolMessage 172
+# AIMessage [观察]
+# 获取到用户的身高为172厘米。
+#
+# [思考]
+# 现在已知体重为90千克，身高为172厘米。根据BMI公式：
+# $$ \text{BMI} = \frac{90}{(1.72)^2} \approx 30.3 $$
+# 计算得出用户的BMI约为30.3，属于肥胖范围（BMI ≥ 30）。
+```
+
+<br>
+
+---
 
 
 
